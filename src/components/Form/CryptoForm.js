9b1f403735e04dc0d653d637f7aaec67535d8599
saman rahgozar <hedayati.asp@gmail.com>
2023-03-31 13:34:01 +0330

    Handling Form Submission

--- src/components/Form/CryptoForm.js
+++ src/components/Form/CryptoForm.js
@@ -1,73 +1,49 @@
 import React, { useState } from "react";
 
 export default function CryptoForm() {
-  // const [enteredName, setEnteredName] = useState("");
-  // const [enteredVol, setEnteredVol] = useState("");
-  // const [enteredDailyRate, setEnteredDailyRate] = useState("");
-  // const [enteredPrice, setEnteredPrice] = useState("");
-  // const [enteredLogo, setEnteredLogo] = useState("");
-  // const [enteredWeeklyRate, setEnteredWeeklyRate] = useState("");
-
-  const [userInput, setUserInput] = useState({
-    enteredName: "",
-    enteredVol: "",
-    enteredDailyRate: "",
-    enteredPrice: "",
-    enteredLogo: "",
-    enteredWeeklyRate: "",
-  });
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredVol, setEnteredVol] = useState("");
+  const [enteredDailyRate, setEnteredDailyRate] = useState("");
+  const [enteredPrice, setEnteredPrice] = useState("");
+  const [enteredLogo, setEnteredLogo] = useState("");
+  const [enteredWeeklyRate, setEnteredWeeklyRate] = useState("");
 
   const nameChangeHandler = (event) => {
-    // setEnteredName(event.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredName: event.target.value,
-    // });
-
-    setUserInput((previousState) => {
-      return { ...previousState, enteredName: event.target.value };
-    });
+    setEnteredName(event.target.value);
   };
   const volChangeHandler = (event) => {
-    //setEnteredVol(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredVol: event.target.value,
-    });
+    setEnteredVol(event.target.value);
   };
   const dailyRateChangeHandler = (event) => {
-    // setEnteredDailyRate(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredVol: event.target.value,
-    });
+    setEnteredDailyRate(event.target.value);
   };
   const priceChangeHandler = (event) => {
-    // setEnteredPrice(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredPrice: event.target.value,
-    });
+    setEnteredPrice(event.target.value);
   };
   const logoChangeHandler = (event) => {
-    // setEnteredLogo(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredLogo: event.target.value,
-    });
+    setEnteredLogo(event.target.value);
   };
 
   const weeklyRateChangeHandler = (event) => {
-    // setEnteredWeeklyRate(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredWeeklyRate: event.target.value,
-    });
+    setEnteredWeeklyRate(event.target.value);
+  };
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+    const cryptoData = {
+      name: enteredName,
+      vol: enteredVol,
+      dailyRate: enteredDailyRate,
+      price: enteredPrice,
+      logo: enteredLogo,
+      weeklyRate: enteredWeeklyRate,
+    };
+    console.log(cryptoData);
   };
 
   return (
     <>
-      <form class="pt-6 w-full max-w-lg" id="fff">
+      <form class="pt-6 w-full max-w-lg" id="fff" onSubmit={submitFormHandler}>
         <div class="mx-4 flex flex-wrap">
           <div class="xl:w-2/12 pr-4 mt-2">
             <input
@@ -123,7 +99,7 @@ export default function CryptoForm() {
           <div class="xl:w-4/12 px-4 mb-2 md:mb-0">
             <button
               className="text-lightBlue-500 bg-transparent border border-solid border-lightBlue-500 hover:bg-lightBlue-500 hover:text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-              type="button"
+              type="submit"
             >
               Add Crypro
             </button>
