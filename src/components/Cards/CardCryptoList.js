7a38621027e3440c5038f5385f17c3bb1fa67d93
saman rahgozar <hedayati.asp@gmail.com>
2023-04-02 19:53:11 +0330

    Understanding Keys

--- src/components/Cards/CardCryptoList.js
+++ src/components/Cards/CardCryptoList.js
@@ -3,11 +3,13 @@ import React from "react";
 import CardCrypto from "components/Cards/CardCrypto.js";
 
 export default function CardCryptoList(props) {
+  console.log("props", props);
   return (
     <>
       <div className="flex flex-wrap mt-4">
         {props.items.map((i) => (
           <CardCrypto
+            key={i.id}
             currency={i.currency}
             vol={i.vol}
             dayRate={i.dayRate}
