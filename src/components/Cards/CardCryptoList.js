33297b35eb37a5c37c842e07c292b7c3bfb7c9ba
saman rahgozar <hedayati.asp@gmail.com>
2023-04-02 20:06:59 +0330

    Outputting Conditional Content

--- src/components/Cards/CardCryptoList.js
+++ src/components/Cards/CardCryptoList.js
@@ -3,22 +3,23 @@ import React from "react";
 import CardCrypto from "components/Cards/CardCrypto.js";
 
 export default function CardCryptoList(props) {
-  console.log("props", props);
+  let cryptoConent = <p>there is no any crypto</p>;
+  if (props.items.length > 0) {
+    cryptoConent = props.items.map((i) => (
+      <CardCrypto
+        key={i.id}
+        currency={i.currency}
+        vol={i.vol}
+        dayRate={i.dayRate}
+        price={i.price}
+        img={i.img}
+        weekRate={i.weekRate}
+      />
+    ));
+  }
   return (
     <>
-      <div className="flex flex-wrap mt-4">
-        {props.items.map((i) => (
-          <CardCrypto
-            key={i.id}
-            currency={i.currency}
-            vol={i.vol}
-            dayRate={i.dayRate}
-            price={i.price}
-            img={i.img}
-            weekRate={i.weekRate}
-          />
-        ))}
-      </div>
+      <div className="flex flex-wrap mt-4">{cryptoConent}</div>
     </>
   );
 }
